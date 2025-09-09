228dbda58b9ecc14b367917a712298b74e3e8f6a
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-09-09 21:20:35 +0500

    Add files via upload

---
+++ p2_pair_with_target.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+//This code is written by Karan Mashru
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n, s, i, j, f=0;
+
+    //This code is written by Karan Mashru
+    cin>>n;
+
+    int a[n];
+
+    //This code is written by Karan Mashru
+    for(i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+
+    cin>>s;
+
+    // Sort the array for two-pointer technique
+    sort(a, a+n);
+
+    //This code is written by Karan Mashru
+    i=0; j=n-1;
+   
+    while(i<j)
+    {
+        if(a[i]+a[j]==s){
+            f=1;
+            break;
+        }else if(a[i]+a[j]>s){
+            j--;
+        }else{
+            i++;
+        }
+    }
+
+    //This code is written by Karan Mashru
+    if(f==0){
+        cout<<"NO\n";
+    }else{
+        cout<<"YES\n";
+    }
+}
