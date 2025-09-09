2a523537655f1c015526ff43a4ab0de7354afc36
Zahid Mehmood <31436393+IamZahidMehmood@users.noreply.github.com>
2025-09-09 21:20:05 +0500

    Add files via upload

--- overflow.cpp
+++ overflow.cpp
@@ -1,18 +1,12 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int a1 = 10000000, a2 = 10000000;
-    int ans;
-    long s = 100000000000000, ans1;
+    long long a1 = 10000000, a2 = 10000000;
+    long long s = 100000000000000LL;
 
-    ans1 = s;
-    ans = s;   // Will overflow since `ans` is int
-
-    long b1 = a1, b2 = a2;
-    ans1 = b1 * b2;
+    long long b1 = a1, b2 = a2;
+    long long ans1 = b1 * b2;
 
     cout << ans1 << "\n";
-
-    return 0;
 }
